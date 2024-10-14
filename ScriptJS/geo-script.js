84616bcbaf23f82135ff0f32414de3ebb741c9f5
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2024-10-14 18:35:53 +0200

    confettis

--- ScriptJS/geo-script.js
+++ ScriptJS/geo-script.js
@@ -5,13 +5,14 @@ const quizzes = {
   historyQuiz: [],
 };
 
-//TODO: In my JS getChoicesButton(), stopQuizz and rejouerButton, setNameUser in LocalStorage, confettis
+//TODO: In my JS setNameUser in LocalStorage, confettis
 //TODO: In HTML add quizzes buttons choices before the let's go button
 //TODO: In JS general add eventListener on quizes choices links of the nav to startQuiz(nameOfTheQuiz) + let underline on clicked link
 
 const USER_CHOICE_INDICATOR = "user-choice"; //conventions => for all importants constants who doesn't change -> in capital
 //and avoid to copy paste the new value if it changes in all the programm
 //and allow devs to use it in the whole programm
+
 export function startQuiz(quizName) {
   // VARIABLES DEFINITIONS
   const currentQuizData = quizzes[quizName];
@@ -23,15 +24,27 @@ export function startQuiz(quizName) {
   const validationButton = document.querySelector(".validationButton");
   const currentPicture = document.querySelector(".image-quest");
   const cardQuest = document.querySelector(".card-quest");
+  const replayButton = document.querySelector(".replayButton");
+
+  const cardScore = document.querySelector(".card-score");
+  const scoreContainer = document.querySelector("#userScore");
+  const scorePlace = scoreContainer.querySelector("span");
+  console.log({ scoreplace: scorePlace });
+
+  //HELPER
+  const getChoicesButtons = () => document.querySelectorAll(".choice");
 
   let currentQuestionIndex = 0;
   let currentQuestion = currentQuizData[currentQuestionIndex];
   let choiceUser;
   let score = 0;
-  homeSection.classList.add("displaynone");
-  questSection.classList.remove("displaynone");
-  scoreSection.classList.add("displaynone");
-  validationButton.classList.remove("displaynone");
+
+  const displayQuest = () => {
+    homeSection.classList.add("displaynone");
+    questSection.classList.remove("displaynone");
+    scoreSection.classList.add("displaynone");
+    validationButton.classList.remove("displaynone");
+  };
 
   const addListenerOnChoicesButtons = () => {
     const choices = document.querySelectorAll(".choice");
@@ -56,23 +69,26 @@ export function startQuiz(quizName) {
       nextButton.remove();
     }
     currentQuestion = currentQuizData[questionIndex];
-    validationButton.classList.remove("displaynone");
-
-    const choicesWrapper = document.querySelector(".choices-wrapper");
-    currentQuestion.answers.forEach((answer) => {
-      const choiceButton = document.createElement("button");
-      choiceButton.classList.add("choice");
-      choiceButton.textContent = answer.choice;
-      choicesWrapper.append(choiceButton);
-    });
-
-    addListenerOnChoicesButtons();
-    currentPicture.src = currentQuestion.picture;
-    currentQuestionIndex++;
+    console.log({ lengthData: currentQuizData.length });
+    console.log({ lengthQuestionPassed: questionIndex });
+    if (questionIndex !== currentQuizData.length) {
+      validationButton.classList.remove("displaynone");
+      const choicesWrapper = document.querySelector(".choices-wrapper");
+      currentQuestion.answers.forEach((answer) => {
+        const choiceButton = document.createElement("button");
+        choiceButton.classList.add("choice");
+        choiceButton.textContent = answer.choice;
+        choicesWrapper.append(choiceButton);
+      });
+      addListenerOnChoicesButtons();
+      currentPicture.src = currentQuestion.picture;
+      currentQuestionIndex++;
+    } else {
+      stopQuiz();
+    }
   };
 
-  const giveCorrection = () => {
-    const choices = document.querySelectorAll(".choice");
+  const giveCorrection = (choices) => {
     const goodAnswer = currentQuestion.answers.find(
       (response) => response.correct === true
     );
@@ -90,7 +106,7 @@ export function startQuiz(quizName) {
   };
 
   const handleClickOnNextButton = () => {
-    const choices = document.querySelectorAll(".choice");
+    const choices = getChoicesButtons();
     choices.forEach((buttonChoice) => {
       buttonChoice.remove();
     });
@@ -106,11 +122,46 @@ export function startQuiz(quizName) {
     nextButton.addEventListener("click", handleClickOnNextButton);
   };
 
+  //CONFETTIS
+
+  function giveConfettis() {
+    if (canvas) {
+      canvas.remove();
+    }
+    const canvas = document.createElement("canvas");
+    canvas.id = "confetti-canvas";
+    cardScore.appendChild(canvas);
+    const myConfetti = confetti.create(canvas, {
+      resize: true,
+      useWorker: true,
+    });
+    myConfetti({
+      particleCount: 100,
+      spread: 160,
+    });
+  }
+
+  const giveScoreUser = () => {
+    scorePlace.innerHTML = `${score} / ${currentQuizData.length}`;
+    giveConfettis();
+  };
+
+  const stopQuiz = () => {
+    questSection.classList.add("displaynone");
+    scoreSection.classList.remove("displaynone");
+    giveScoreUser();
+  };
+
+  const replayQuiz = () => {
+    startQuiz("geoQuiz");
+  };
+
   // PROGRAM EXECUTION
+  displayQuest();
   goToQuestion(currentQuestionIndex);
   validationButton.addEventListener("click", () => {
-    console.log("CLICK ON VALIDATION");
-    giveCorrection();
-    createNextButton();
+    giveCorrection(getChoicesButtons());
+    createNextButton(getChoicesButtons());
   });
+  replayButton.addEventListener("click", replayQuiz);
 }
